e050838bf7d915da77fc951f992b0314c153b54c
Daniel Kiss <littlesnorrboy@gmail.com>
2020-11-20 22:24:11 +0100

    Wip

--- core/src/collections/mpfifo.rs
+++ core/src/collections/mpfifo.rs
@@ -1,6 +1,6 @@
 use std::{
-    cell::UnsafeCell, marker::Unpin, mem::MaybeUninit, pin::Pin, sync::atomic::spin_loop_hint,
-    sync::atomic::AtomicUsize, sync::atomic::Ordering,
+    cell::UnsafeCell, convert::TryFrom, marker::Unpin, mem::MaybeUninit, pin::Pin,
+    sync::atomic::spin_loop_hint, sync::atomic::AtomicU64, sync::atomic::Ordering,
 };
 
 use super::QueueError;
@@ -9,11 +9,12 @@ type FixMessageBuffer<T> = Pin<Box<[MaybeUninit<T>]>>;
 
 /// Multi-producer FIFO queue
 ///
+// implementation note: timestamps of `_ts` mean that that data is monotonically increasing
 pub struct MpFifo<T: Copy> {
-    next: AtomicUsize,
-    ready: AtomicUsize,
-    head: AtomicUsize,
-    tail: AtomicUsize,
+    next: AtomicU64,
+    ready_ts: AtomicU64,
+    head: AtomicU64,
+    tail: AtomicU64,
 
     size_mask: usize,
     buffer: UnsafeCell<FixMessageBuffer<T>>,
@@ -35,10 +36,10 @@ where
         Ok(Self {
             size_mask: size - 1,
             buffer: UnsafeCell::new(Pin::new(buffer.into_boxed_slice())),
-            head: AtomicUsize::new(0),
-            tail: AtomicUsize::new(0),
-            next: AtomicUsize::new(0),
-            ready: AtomicUsize::new(0),
+            head: AtomicU64::new(0),
+            tail: AtomicU64::new(0),
+            next: AtomicU64::new(0),
+            ready_ts: AtomicU64::new(0),
         })
     }
 
@@ -48,36 +49,34 @@ where
             // begin
             // ensure next is read first
             let next = self.next.load(Ordering::Acquire);
+            let new_next = incr(next, self.size_mask);
             let tail = self.tail.load(Ordering::Acquire);
-            if incr(next, self.size_mask) == tail {
+            if new_next as u64 == tail {
                 return Err(QueueError::Full);
             }
             let n = self
                 .next
-                .compare_and_swap(next, incr(next, self.size_mask), Ordering::Release);
+                .compare_and_swap(next, new_next as u64, Ordering::Release);
             if n != next {
                 // another thread got this slot
-                spin_loop_hint();
+                spin_loop_hint(); // signal the cpu to enter low-power mode
                 continue 'retry;
             }
             // write the value
             unsafe {
                 let slots = &mut *self.buffer.get();
-                slots[next] = MaybeUninit::new(value);
+                slots[next as usize] = MaybeUninit::new(value);
             }
             // finished
             //
             // just incrementing is cheaper than cas and we can calculate its value...
-            let r = self.ready.fetch_add(1, Ordering::Release);
-            let ready = incr(r, self.size_mask);
+            let r = self.ready_ts.fetch_add(1, Ordering::AcqRel);
+            let ready = incr(r, self.size_mask) as u64;
 
             // check for doneness on all threads
             if ready == self.next.load(Ordering::Acquire) {
                 // all concurrent writes finished, update head
                 self.head.store(ready, Ordering::Release);
-                // try to reset the 'ready' counter to this actual position
-                // but it should be fine if this fails
-                self.ready.compare_and_swap(r, ready, Ordering::Relaxed);
             }
 
             return Ok(());
@@ -95,42 +94,22 @@ where
         // position
         let value = unsafe {
             let slots = &*self.buffer.get();
-            slots[tail].assume_init()
+            slots[tail as usize].assume_init()
         };
         self.tail
-            .store(incr(tail, self.size_mask), Ordering::Release);
+            .store(incr(tail, self.size_mask) as u64, Ordering::Release);
         Some(value)
     }
 
     /// Pop for when multiple consumers exist
     pub fn pop_multi(&self) -> Option<T> {
-        loop {
-            let tail = self.tail.load(Ordering::Acquire);
-            let head = self.head.load(Ordering::Acquire);
-            if tail == head {
-                // empty
-                return None;
-            }
-            let item = unsafe {
-                let slots = &*self.buffer.get();
-                slots[tail].assume_init()
-            };
-
-            let res =
-                self.tail
-                    .compare_and_swap(tail, incr(tail, self.size_mask), Ordering::Release);
-            if res == tail {
-                return Some(item);
-            }
-            // else another thread stole this item, try again
-            spin_loop_hint();
-        }
+        todo!()
     }
 }
 
 #[inline]
-fn incr(val: usize, mask: usize) -> usize {
-    (val + 1) & mask
+fn incr(val: u64, mask: usize) -> usize {
+    (usize::try_from(val).unwrap() + 1) & mask
 }
 
 #[cfg(test)]
@@ -138,18 +117,20 @@ mod tests {
     use crate::MessageId;
 
     use super::*;
-    use std::{sync::Arc, sync::Barrier};
+    use std::{sync::Arc, sync::Barrier, time::Duration, time::Instant};
 
     #[test]
     #[cfg_attr(miri, ignore)]
     fn basic_push_pop() {
-        let queue: MpFifo<MessageId> = MpFifo::new(16).unwrap(); // lot smaller capacity than items we push
+        const PRODUCERS: u64 = 8;
+
+        let queue: MpFifo<MessageId> = MpFifo::new(16).unwrap();
         let queue = Arc::new(queue);
 
-        let bar = Arc::new(Barrier::new(5));
+        let bar = Arc::new(Barrier::new(PRODUCERS as usize + 1));
         let producers = {
-            let mut producers = Vec::with_capacity(4);
-            for i in 0..4 {
+            let mut producers = Vec::with_capacity(PRODUCERS as usize);
+            for i in 0..PRODUCERS {
                 producers.push({
                     let queue = Arc::clone(&queue);
                     let bar = Arc::clone(&bar);
@@ -164,35 +145,38 @@ mod tests {
                                 }
                             }
                         }
-                        println!("thread {} exit", i);
+                        eprintln!("thread {} exit", i);
                     })
                 })
             }
             producers
         };
 
-        let mut seen = vec![0; 100 * 4];
-        let mut it = 0;
+        let mut seen = vec![0; 100 * PRODUCERS as usize];
+        let mut it = 0u64;
         let mut pops = 0;
         bar.wait();
-        while pops < 100 * 4 {
+        let start = Instant::now();
+        while pops < 100 * PRODUCERS && (Instant::now() - start) < Duration::from_secs(3) {
+            // pretty big leeway....
             it += 1;
-            let msg = queue.pop_multi();
+            let msg = queue.pop_single();
             if let Some(msg) = msg {
-                assert!(msg.0 < 100 * 4);
+                assert!(msg.0 < 100 * PRODUCERS);
                 seen[msg.0 as usize] += 1;
                 pops += 1;
             }
         }
         println!("Pops: {} It: {}", pops, it);
-        let seen_all = seen.iter().all(|x| *x == 1);
-        assert!(
-            seen_all,
-            "{:?}",
-            seen.iter().enumerate().collect::<Vec<_>>(),
-        );
-
+        let invalid = seen
+            .iter()
+            .enumerate()
+            .filter(|(_, x)| **x != 1)
+            .collect::<Vec<_>>();
+        let seen_all = invalid.is_empty();
+        assert!(seen_all, "Invalid items: {}\n{:?}", invalid.len(), invalid);
         for producer in producers {
+            producer.thread();
             producer.join().unwrap();
         }
     }
